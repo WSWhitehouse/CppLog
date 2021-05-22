1dce3957767d36297b4a7ee8cac156b125004c25
WSWhitehouse <williamswhitehouse@gmail.com>
2021-05-22 16:07:49 +0100

    Add simple log test

---
+++ log/log.h
@@ -0,0 +1,14 @@
+#ifndef CPP_LOG_H
+#define CPP_LOG_H
+
+#include <iostream>
+#include <string>
+
+namespace Log
+{
+  static inline str(const std::string& _str)
+  {
+    std::cout << _str << std::endl;
+  }
+}
+#endif // CPP_LOG_H
